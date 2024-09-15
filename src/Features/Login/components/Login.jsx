34f24cb794f1784a13433dbaf2e830377b85e756
Your Name <your.email@example.com>
2024-09-16 01:58:21 +0530

    changes 16/9 sneha

--- src/Features/Login/components/Login.jsx
+++ src/Features/Login/components/Login.jsx
@@ -1,99 +1,130 @@
-import React, { useEffect, useRef } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useStateContext } from '../contexts/StateContext';
 import { useMutation } from '@tanstack/react-query';
 import { authLogin } from '../services/auth-api';
-import axios from 'axios';
 import axiosClient from '../../../axios-client';
 
 const Login = () => {
-
   const { setUser, setToken } = useStateContext();
   const usernameRef = useRef();
-  const passwordRef = useRef()
+  const passwordRef = useRef();
   const navigate = useNavigate();
 
   const loginMutation = useMutation({
     mutationFn: authLogin,
     onSuccess: (response) => {
-      console.log(response)
-      setUser(response.data)
-      setToken(response.token)
-      navigate('/home')
+      console.log(response);
+      setUser(response.data);   // Ensure response.data contains the correct user data.
+      setToken(response.token); // Ensure response.token contains the correct token.
+      navigate('/home');
     },
     onError: (err) => {
-      console.log(err)
-    }
-  })
-  const handleSubmit = (e) => {
-    // console.log("hello signin")
-    // e.preventDefault();
-    // console.log(usernameRef.current.value, passwordRef.current.value)
+      console.log('Login failed:', err);
+    },
+  });
 
-    // const payload = {
-    //   username: usernameRef.current.value,
-    //   password: passwordRef.current.value
-    // };
-    // loginMutation.mutate(payload)
+  const handleSubmit = (e) => {
+    e.preventDefault();  // Prevent page refresh
+
+    // Get the values from input refs
+    const payload = {
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    // Trigger the login mutation with the payload
+    loginMutation.mutate(payload);
+  };
+
+
+  const handleLogin=()=>{
+    axiosClient.get("/home").then(res=>{
+      console.log(res.data);
+    }).catch(err=>{
+      console.log(err);
+      
+    })
   }
-  useEffect(()=>{
-    axiosClient.post("/auth/login",
-      {
-        "username":"Shramana",
-        "password":"password"
-      }
-    )
-    .then((res)=>{
-      console.log(res)
-    })
-    .catch((error)=>{
-      if(error.response){
-        console.log(error.response.status)
-      }
-      console.log(error)
-    })
-  },[])
+
   return (
     <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        {/* <a href="#" className="flex items-center mb-6 text-3xl font-bold text-grey-700">
-          WishBox   
-      </a> */}
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign in to your account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="POST" onSubmit={handleSubmit}>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
-                <input type="text" name="username" ref={usernameRef} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required="" />
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Your username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  ref={usernameRef}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Username"
+                  required
+                />
               </div>
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" name="password" ref={passwordRef} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  ref={passwordRef}
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
               </div>
               <div className="flex items-center justify-between">
                 <div className="flex items-start">
                   <div className="flex items-center h-5">
-                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                    />
                   </div>
                   <div className="ml-3 text-sm">
-                    <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                    <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">
+                      Remember me
+                    </label>
                   </div>
                 </div>
-                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-white">Forgot password?</a>
+                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-white">
+                  Forgot password?
+                </a>
               </div>
-              <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+              <button
+                type="submit"
+                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Sign in
+              </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don't have an account yet? <Link to="/register" className="font-medium text-primary-600 hover:underline dark:text-white">Sign up</Link>
+                Don't have an account yet?{' '}
+                <Link to="/register" className="font-medium text-primary-600 hover:underline dark:text-white">
+                  Sign up
+                </Link>
+
               </p>
             </form>
+
+            <button onClick={handleLogin}>
+              Login
+            </button>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/axios-client.jsx
+++ src/axios-client.jsx
@@ -1,29 +1,37 @@
 import axios from "axios"
 
-const axiosClient= axios.create({
-    baseURL:`${import.meta.env.VITE_APP_BASE_URL}/api`
+//console.clear()
+const axiosClient = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
 })
+let ApiCallCount=0
+axiosClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('ACCESS_TOKEN')
+    config.headers.Authorization = `Bearer ${token}`
+    // config.headers.Accept=application/json
+    // config.headers["Content-Type"]=application/json
 
-axiosClient.interceptors.request.use((config)=>{
-    const token= localStorage.getItem('ACCESS_TOKEN')
-    config.headers.Authorization=`Bearer ${token}`
+    console.log("Api Call Count: ",config.url,ApiCallCount++);
     return config
 })
 
-axiosClient.interceptors.response.use((response)=>{
+
+axiosClient.interceptors.response.use((response) => {
+    console.log("Response: ",response);
     return response
 },
-    (error)=>{
-        try{
-            const {response}=error
-            if(response.status===401){
+    (error) => {
+        try {
+
+            const { response } = error
+
+            if (response.status === 401) {
                 localStorage.removeItem('ACCESS_TOKEN')
             }
-        }
-        catch(e){
+        } catch (e) {
             console.log(e);
+
         }
         throw error;
-    }
-)
-export default axiosClient
+    })
+export default axiosClient;
